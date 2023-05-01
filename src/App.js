8b4924fc1c43cdac38168c6b70ab83c3aa3e56aa
Dhananjay Deore <dhananjaydeore@Dhananjays-MacBook-Air.local>
2023-05-02 00:43:59 +0530

    add files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Editor from "./components/Editor";
+import { useState, useEffect } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
 function App() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+    <html>${html}</html>
+    <style>${css}</style>
+    <script>${js}</script>
+  
+    `);
+    }, 1000);
+    // return clearTimeout(timeout);
+  }, [html, css, js]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JavaScript"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          height="100%"
+          width="100%"
+          border="0"
+        />
+      </div>
+    </>
   );
 }
 
